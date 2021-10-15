9631aa48546f05472160dbaaa1aa1be4da830ac3
Bruno Mateus <brunomateus@gmail.com>
2021-10-15 00:39:37 -0300

    Adicionando dados sobre o envio da primeira lista

--- assets/js/sheetApi.js
+++ assets/js/sheetApi.js
@@ -3,15 +3,15 @@ document.addEventListener('readystatechange', event => {
     // When window loaded ( external resources are loaded too- `css`,`src`, etc...) 
     if (event.target.readyState === "complete") {
        
-        const appUrl = ""
+        const appUrl = "https://script.google.com/macros/s/AKfycby8Cc77YPc4NfxcGTUz7Ba6n0gdBA_s7JkvdeJ8LPU_646M-LGcaVia1LozCFcUTqAhrQ/exec"
         const urls = [
+        "10NrNj2BWTUxd8fNMSEzAuzHobY6bqYPDBHmSENAR4OA"
         ]
         
         urls.forEach(function(sheetUrl, index) {
             const url = appUrl + "?id=" + sheetUrl 
 
             if(sheetUrl != ""){
-                console.log(sheetUrl + " " + index)
                 fetch(url)
                 .then(data => { return data.json()})
                 .then(res => { document.getElementById("pp" + (index + 1)).innerHTML = res })
